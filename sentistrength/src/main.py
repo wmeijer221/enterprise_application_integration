26694e8a966e5d076993eea3aa4b3959472c1151
Willem Meijer <70508656+wmeijer221@users.noreply.github.com>
2022-11-29 10:43:54 +0100

    removed debug line

--- sentistrength/src/main.py
+++ sentistrength/src/main.py
@@ -3,5 +3,4 @@ from wrapper import SentistrengthWrapper
 
 with SentistrengthWrapper() as wrapper: 
     adapter = SentistrengthAdapter(wrapper)
-    print(wrapper.get_sentiment("Some\nillegal\rstring"))
 
