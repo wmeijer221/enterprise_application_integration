7d10a14e9d83bfe416fb34eea48fa0c4b2f3c2d9
Willem Meijer <w.meijer.5@student.rug.nl>
2022-12-14 17:50:12 +0100

    added periodic retrieval

--- endpoint_adapters/endpoint_adapters/title_broker.py
+++ endpoint_adapters/endpoint_adapters/title_broker.py
@@ -1,3 +1,6 @@
+from os import getenv
+import time
+
 from endpoint_adapters.adapters import APIAdapter
 
 
@@ -7,13 +10,18 @@ class TitleBroker:
     # TODO: https://trello.com/c/cfROzybd/18-implement-service-for-exploring-considered-movies
 
     DATA_PATH = "./data/movies.txt"
+    INTERVAL_KEY = "UPDATE_INTERVAL"
+    running = True
 
     def __init__(self, receiver: APIAdapter):
         self.receiver = receiver
 
     def start(self):
-        """Starts publishing new titles, and periodically re-."""
+        """Starts publishing new titles, and periodically updates the reviews on those."""
         with open(self.DATA_PATH, "r", encoding="utf-8") as data_file:
             titles = [title.strip() for title in data_file.readlines()]
+        update_interval = int(getenv(self.INTERVAL_KEY))
+        while self.running:
             self.receiver.set_list_of_titles(titles)
             self.receiver.fetch()
+            time.sleep(update_interval)
