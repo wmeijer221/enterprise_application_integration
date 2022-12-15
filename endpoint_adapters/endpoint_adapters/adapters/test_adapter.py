83d7e8f34dc104e39630f8a2aa7c13060f78a692
Willem Meijer <w.meijer.5@student.rug.nl>
2022-12-15 09:38:25 +0100

    removed dead test code

--- endpoint_adapters/endpoint_adapters/adapters/test_adapter.py
+++ endpoint_adapters/endpoint_adapters/adapters/test_adapter.py
@@ -7,9 +7,6 @@ from endpoint_adapters.adapters import APIAdapter
 class TestAdapter(APIAdapter):
     """Test adapter that publishes a fake review periodically."""
 
-    SLEEP_TIME = 1
-    is_running = True
-
     def fetch(self):
         for title in self._list_of_titles:
             review_text = "Did enjoy this, but having read the book by Agatha Christie I can say that you should read the book instead. For a 2 hour movie it does not develop the characters or story very well but cinematography and direction is spectacular."
