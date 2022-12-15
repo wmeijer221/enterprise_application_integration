cc5cd5fff7607a1d5c079f760ca4b7d61e100392
Willem Meijer <w.meijer.5@student.rug.nl>
2022-12-15 09:05:16 +0100

    added retry-circuit breaker to queue

--- endpoint_adapters/endpoint_adapters/queue_publisher.py
+++ endpoint_adapters/endpoint_adapters/queue_publisher.py
@@ -1,10 +1,11 @@
 from os import getenv
-
+import time
+from socket import gaierror
 import logging
 import pika
 
-from endpoint_adapters.model.channel_message import ChannelMessage
 
+from endpoint_adapters.model.channel_message import ChannelMessage
 from endpoint_adapters.utils.json_helper import to_json
 
 CHANNEL_NAME_KEY = "CHANNEL_NAME"
@@ -16,11 +17,12 @@ ROUTING_KEY_KEY = "ROUTING_KEY"
 class QueuePublisher:
     """Generic interface to publish to a message queue."""
 
+    MAX_RETRIES = 5
+    TIMEOUT = 10
+
     def __init__(self):
         channel_name = getenv(CHANNEL_NAME_KEY)
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=channel_name)
-        )
+        self.connection = self.__try_connect(channel_name)
         self.channel = self.connection.channel()
         self.queue_name = getenv(QUEUE_NAME_KEY)
         self.channel.queue_declare(queue=self.queue_name)
@@ -28,6 +30,21 @@ class QueuePublisher:
         logging.info(
             f"Initialized queue publisher with: {channel_name=}, queue_name={self.queue_name}, exchange={self.exchange}"
         )
+
+    def __try_connect(self, channel_name: str):
+        """Implements connecting with retry-circuit breaker pattern."""
+        for tries in range(1, self.MAX_RETRIES + 1):
+            try:
+                connection = pika.BlockingConnection(
+                    pika.ConnectionParameters(host=channel_name)
+                )
+                return connection
+            except gaierror:
+                logging.error("Could not connect with channel %s for the %s/%s time. Retrying in %s seconds.", channel_name, tries, self.MAX_RETRIES, self.TIMEOUT)
+                if tries >= self.MAX_RETRIES:
+                    raise
+                time.sleep(self.TIMEOUT)
+        raise Exception("Illegal state; this should not be reached.")
 
     def publish(self, message: ChannelMessage):
         """Publishes the message to the channel."""
