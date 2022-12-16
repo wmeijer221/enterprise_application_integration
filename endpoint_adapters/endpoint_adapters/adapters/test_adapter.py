1b29826ec1e1ed632f4975b45b0a2027ef6cfd9a
Willem Meijer <w.meijer.5@student.rug.nl>
2022-12-16 10:19:51 +0100

    test reviews have an index

--- endpoint_adapters/endpoint_adapters/adapters/test_adapter.py
+++ endpoint_adapters/endpoint_adapters/adapters/test_adapter.py
@@ -8,13 +8,13 @@ class TestAdapter(APIAdapter):
     """Test adapter that publishes a fake review periodically."""
 
     def fetch(self):
-        for title in self._list_of_titles:
-            review_text = "Did enjoy this, but having read the book by Agatha Christie I can say that you should read the book instead. For a 2 hour movie it does not develop the characters or story very well but cinematography and direction is spectacular."
+        for index, title in enumerate(self._list_of_titles):
+            review_text = f"Did enjoy this, but having read the book by Agatha Christie I can say that you should read the book instead. For a 2 hour movie it does not develop the characters or story very well but cinematography and direction is spectacular."
             review = Review(
                 title,
                 review_text,
                 source_name="test",
-                source_id=0,
+                source_id=index,
                 timestamp=datetime.datetime.now(),
                 reviewer="Edgar Allan Poe",
             )
