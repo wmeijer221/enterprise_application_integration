93fc3fcf20cc290efde6c06f9958ddfc9620770e
Willem Meijer <w.meijer.5@student.rug.nl>
2023-01-09 14:47:08 +0100

    project setup

---
+++ title_finder/title_finder/__main__.py
@@ -0,0 +1,29 @@
+import logging 
+logging.basicConfig(level=logging.WARNING)
+
+# Imports the base module
+import importlib
+try:
+    # According to the docker container structure.
+    base = importlib.import_module("base", "..base")
+except:
+    # According to the repository structure in case it's ran locally.
+    import sys
+    logging.warning("Failed to load base, trying alternative source.")
+    spec = importlib.util.spec_from_file_location("base", "../base/base/__init__.py")
+    base = importlib.util.module_from_spec(spec)
+    sys.modules["base"] = base
+    spec.loader.exec_module(base)
+from base._version import VERSION as BASE_VERSION
+
+from title_finder._version import VERSION
+
+logo = f"""
+  _______ _ _   _        ______ _           _           
+ |__   __(_) | | |      |  ____(_)         | |          
+    | |   _| |_| | ___  | |__   _ _ __   __| | ___ _ __ 
+    | |  | | __| |/ _ \ |  __| | | '_ \ / _` |/ _ \ '__|
+    | |  | | |_| |  __/ | |    | | | | | (_| |  __/ |   
+    |_|  |_|\__|_|\___| |_|    |_|_| |_|\__,_|\___|_|   
+                    (v{VERSION} with base v{BASE_VERSION})                                                   
+"""
