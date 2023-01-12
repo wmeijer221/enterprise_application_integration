2f959227966e2e53d1b43ee2cbed507f8f4c6a7d
Nik <nikdijkema@hotmail.com>
2023-01-12 23:10:21 +0100

    StorageService callback function refactored

--- storage_service/storage_service/storage_service.py
+++ storage_service/storage_service/storage_service.py
@@ -3,12 +3,13 @@ import json
 from bson import json_util
 from os import getenv
 from storage_service.mongo_db import MongoDB
+from base.canonical_model.title import Title
 from base.canonical_model.review import Review
 from base.canonical_model.review_sentiment import ReviewSentiment
 from base.channel_messaging import ChannelMessage, create_connection, receive_from_queue, publish_to_queue, start_consuming
 from storage_service._version import VERSION
+from functools import partial, partialmethod
 
-LOGGER = logging.getLogger(__name__)
 
 MONGO_DB_HOST_KEY = "MONGO_DB_HOST"
 MONGO_DB_PORT_KEY = "MONGO_DB_PORT"
@@ -17,6 +18,8 @@ MONGO_DB_PASSWORD_KEY = "MONGO_DB_PASSWORD"
 MONGO_DB_DATABASE_KEY = "MONGO_DB_DATABASE"
 MONGO_DB_AUTHSOURCE_KEY = "MONGO_DB_AUTHSOURCE"
 RABBIT_MQ_HOST_KEY = "RABBIT_MQ_HOST"
+BASIC_UPSERT_QUEUES_IN_KEY = "BASIC_UPSERT_QUEUES_IN" 
+NEW_TITLE_IN_KEY = "NEW_TITLE_IN"
 NEW_REVIEW_IN_KEY = "NEW_REVIEW_IN"
 NEW_SENTIMENT_IN_KEY = "NEW_SENTIMENT_IN"
 
@@ -35,22 +38,60 @@ class StorageService:
 
         # RabbitMQ instance
         rabbit_mq_host = getenv(RABBIT_MQ_HOST_KEY)
+        # self.basic_upsert_queues_in = json.loads(getenv(BASIC_UPSERT_QUEUES_IN_KEY))
+        new_title_in = getenv(NEW_REVIEW_IN_KEY)
         new_review_in = getenv(NEW_REVIEW_IN_KEY)
         new_sentiment_in = getenv(NEW_SENTIMENT_IN_KEY)
         create_connection(rabbit_mq_host)
-        receive_from_queue(new_review_in, self.__new_review_callback, Review)
-        receive_from_queue(new_sentiment_in, self.__new_sentiment_callback, ReviewSentiment)
+
+        # self.__callback_functions = {}
+        # for queue in self.basic_upsert_queues_in:
+        #     self.__callback_functions[queue] = partialmethod(
+        #     self.__basic_match_upsert_,
+        #     collection='',
+        #     match_fields={
+        #             "uuid": "uuid",
+        #             "title": "title"
+        #             }),
+        #     receive_from_queue(queue, self.__callback_functions[queue])
+
+        self.__new_title_callback = partial(
+            StorageService.__basic_match_upsert_,
+            self,
+            collection='titles',
+            match_fields={
+                "uuid": "uuid",
+                "title": "title"
+            })
+        self.__new_review_callback = partial(
+            StorageService.__basic_match_upsert_,
+            self,
+            collection='reviews',
+            match_fields={
+                "uuid": "uuid",
+            })
+        self.__new_sentiment_callback = partial(
+            StorageService.__basic_match_upsert_,
+            self,
+            collection='reviews',
+            match_fields={
+                "uuid": "review_uuid"
+            })
+        receive_from_queue('new_title', self.__new_title_callback, Title)
+        receive_from_queue('new_review', self.__new_review_callback, Review)
+        receive_from_queue('new_sentiment', self.__new_sentiment_callback, ReviewSentiment)
         start_consuming()
 
-    def __new_review_callback(self, message: ChannelMessage):
+    def __basic_match_upsert_(self, message: ChannelMessage, collection: str, match_fields:dict = {}):
+        logging.info(f'Received message: {message.body}')
         payload_dict = vars(message.body)
-        inserted_id = self.mongo.getDB().reviews.insert_one(payload_dict).inserted_id
-        LOGGER.debug(f"Inserted document {inserted_id} in MongoDB")
-
-    def __new_sentiment_callback(self, message: ChannelMessage):
-        payload_dict = vars(message.body)
-        review_uuid = payload_dict.pop("review_uuid")
-        sentiment_dict = {"sentiment": payload_dict}
-        filter_query = {"uuid": review_uuid}
-        update_result = self.mongo.getDB().reviews.update_one(filter_query, {"$set": sentiment_dict})
-        LOGGER.debug(f"Added sentiment to {update_result.modified_count} document(s) of {update_result.matched_count} matches in MongoDB")
+        filter_query = {}
+        if match_fields:
+            for collection_match_field, message_match_field in match_fields.items():
+                message_match_value = payload_dict[message_match_field]
+            filter_query[collection_match_field] = message_match_value
+        update_result = self.mongo.getDB()[collection].update_one(filter_query, {"$set": payload_dict}, upsert=True)
+        if update_result.upserted_id:
+            logging.info(f'Upserted document {update_result.upserted_id} in collection {collection} in MongoDB')
+        else:
+            logging.info(f'Updated {update_result.modified_count} document(s) of {update_result.matched_count} matches in collection {collection} MongoDB')
