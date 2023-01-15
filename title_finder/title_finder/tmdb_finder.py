19fc80081e88fc1136824de60d1da6b09cf38904
Willem Meijer <w.meijer.5@student.rug.nl>
2023-01-15 10:19:31 +0100

    title finder uses uuid3 for title IDs.

--- title_finder/title_finder/tmdb_finder.py
+++ title_finder/title_finder/tmdb_finder.py
@@ -4,7 +4,7 @@ import time
 import logging
 import datetime
 import json
-from uuid import uuid4, uuid1
+from uuid import uuid4, uuid1, uuid3
 
 from base.utils.http_status_code_helper import is_success_code
 from base.canonical_model import Title
@@ -82,9 +82,10 @@ class TMDBFinder:
         for entry in data["results"]:
             genres = self.__to_genres(entry)
             cast, crew = self.__get_cast_and_crew(endpoint[0], entry)
+            title_name = self.__get_title(entry)
             new_title = Title(
-                str(uuid4()),
-                self.__get_title(entry),
+                str(uuid3(title_name)),
+                title_name,
                 endpoint[0],
                 genres, cast, crew)
             movies.append(new_title)
